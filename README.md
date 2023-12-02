2b1fe5700e4b3cff05438fffa623303cd8fbda43
Kaden Taylor <kadenjtaylor@gmail.com>
2023-12-02 14:48:15 -0800

    day 1 part 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2023 (Rust)
+
+The goal is to set this up so that `cargo run` produces the answer for the question referenced in the commit.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::i8;
+
+fn first_digit(s: &str) -> Option<char> {
+    s.chars().find(|&c| c.is_digit(10))
+}
+
+// Note: I don't like how similar this is to the above fn
+fn last_digit(s: &str) -> Option<char> {
+    s.chars().rev().find(|&c| c.is_digit(10))
+}
+
+// Note: This is crying out for a for-comprehension
+fn extract_number(s: &str) -> Option<i8> {
+    first_digit(s)
+        .and_then(|a| last_digit(s).and_then(|b| Some((a, b))))
+        .and_then(|(a, b)| (a.to_string() + &b.to_string()).parse().ok())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lines = include_str!("../resources/problem_1.txt").lines();
+    let sum: i32 = lines
+        .map(|s| extract_number(s))
+        .flatten() // this is only okay because we know all the lines have a first/last digit
+        .map(|i| i as i32) // Here we see the cost of the i8 choice
+        .sum();
+    println!("Sum: {}", sum)
 }
