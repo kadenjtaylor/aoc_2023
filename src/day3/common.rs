0b3f5fe7fee75f863fa5bdcebe7a5d947ac93ad0
Kaden Taylor <kadenjtaylor@gmail.com>
2023-12-19 23:17:05 -0800

    refactoring

---
+++ src/day3/common.rs
@@ -0,0 +1,125 @@
+#[derive(Debug)]
+pub struct Cell {
+    pub x: i32,
+    pub y: i32,
+    pub content: char,
+}
+
+#[derive(Debug)]
+pub struct CellGroup {
+    pub cells: Vec<Cell>,
+    pub num: String,
+}
+
+fn retrieve_cells() -> Vec<Cell> {
+    let lines = include_str!("../../resources/problem_3.txt").lines();
+    let mut cells: Vec<Cell> = vec![];
+    for (num, line) in lines.enumerate() {
+        line.chars().enumerate().for_each(|(char_num, c)| {
+            cells.push(Cell {
+                y: num as i32,
+                x: char_num as i32,
+                content: c,
+            })
+        })
+    }
+    cells
+}
+
+fn partition_cell_types(cells: Vec<Cell>) -> (Vec<Cell>, Vec<Cell>, Vec<Cell>) {
+    let mut numbers: Vec<Cell> = vec![];
+    let mut symbols: Vec<Cell> = vec![];
+    let mut empties: Vec<Cell> = vec![];
+    for c in cells {
+        if c.content.is_ascii_digit() {
+            numbers.push(c);
+        } else if c.content == '.' {
+            empties.push(c);
+        } else {
+            symbols.push(c);
+        }
+    }
+    (numbers, symbols, empties)
+}
+
+struct Acc {
+    nums: Vec<CellGroup>,
+    partial: Vec<Cell>,
+}
+
+fn flush(acc: Acc, new_partial: Vec<Cell>) -> Acc {
+    let mut new_cells = vec![];
+    new_cells.extend(acc.partial);
+    let num_str: String = new_cells
+        .iter()
+        .map(|cell: &Cell| cell.content.to_string())
+        .collect::<Vec<String>>()
+        .join("");
+    let mut new_nums = vec![];
+    new_nums.extend(acc.nums);
+    new_nums.extend(vec![CellGroup {
+        cells: new_cells,
+        num: num_str,
+    }]);
+    Acc {
+        nums: new_nums,
+        partial: new_partial,
+    }
+}
+
+fn combine_numbers(mut number_cells: Vec<Cell>) -> Vec<CellGroup> {
+    number_cells.sort_by(|a, b| a.y.cmp(&b.y).then(a.x.cmp(&b.x)));
+    let mut acc = Acc {
+        nums: vec![],
+        partial: vec![],
+    };
+    for next in number_cells {
+        match acc.partial.last() {
+            None => {
+                acc.partial.push(next);
+            }
+            Some(cell) => {
+                if next.x - cell.x == 1 {
+                    acc.partial.push(next);
+                } else {
+                    acc = flush(acc, vec![next]);
+                }
+            }
+        }
+    }
+    acc = flush(acc, vec![]);
+    acc.nums
+}
+
+pub struct Schematic {
+    pub number_groups: Vec<CellGroup>,
+    pub symbols: Vec<Cell>,
+    pub empties: Vec<Cell>,
+}
+
+pub fn produce_schematic() -> Schematic {
+    // Parse the given file into cells
+    let cells = retrieve_cells();
+
+    // Decide which ones are numbers and which are symbols
+    let (numbers, symbols, empties) = partition_cell_types(cells);
+
+    // The schematic is just the above with the numbers all put together
+    Schematic {
+        number_groups: combine_numbers(numbers),
+        symbols,
+        empties,
+    }
+}
+
+fn does_border(g: &CellGroup, cell: &Cell) -> bool {
+    g.cells.iter().any(|gc| {
+        let x_dist = gc.x.abs_diff(cell.x);
+        let y_dist = gc.y.abs_diff(cell.y);
+        x_dist <= 1 && y_dist <= 1
+    })
+}
+
+pub fn is_part(g: &CellGroup, symbols: &Vec<Cell>) -> bool {
+    symbols.iter().any(|c| does_border(g, c))
+}
--- src/day3_part1.rs
+++ src/day3_part1.rs
@@ -1,138 +1,17 @@
-#[derive(Debug)]
-struct Cell {
-    x: i32,
-    y: i32,
-    content: char,
-}
+#[path = "day3/common.rs"]
+mod common;
 
-#[derive(Debug)]
-struct CellGroup<'a> {
-    cells: Vec<&'a Cell>,
-    num: String,
-}
-
-fn partition_cell_types(cells: &Vec<Cell>) -> (Vec<&Cell>, Vec<&Cell>, Vec<&Cell>) {
-    let mut numbers = vec![];
-    let mut symbols = vec![];
-    let mut empties = vec![];
-    cells.iter().for_each(|c| {
-        if c.content.is_ascii_digit() {
-            numbers.push(c);
-        } else if c.content == '.' {
-            empties.push(c);
-        } else {
-            symbols.push(c);
-        }
-    });
-    (numbers, symbols, empties)
-}
-
-struct Acc<'a> {
-    nums: Vec<CellGroup<'a>>,
-    partial: Vec<&'a Cell>,
-}
-
-// TODO: Refactor - this is ugly as hell
-fn combine_numbers(number_cells: Vec<&Cell>) -> Vec<CellGroup> {
-    // println!("Figuring out which cells belong to the same group...");
-    // println!("They'll be equal in y, and 1 apart in x");
-    let mut my_cells = number_cells.clone();
-    my_cells.sort_by(|&a, &b| a.y.cmp(&b.y).then(a.x.cmp(&b.x)));
-    let mut acc = Acc {
-        nums: vec![],
-        partial: vec![],
-    };
-    my_cells.iter().for_each(|next| {
-        // println!("Accumulating: {:?}", next);
-        if acc.partial.is_empty() {
-            acc.partial.extend(vec![next]);
-            // println!("Added {:?} to partial", next);
-        } else if let Some(cell) = acc.partial.last() {
-            // println!("{:?} already in partial... now what?", cell);
-            if next.x - cell.x == 1 {
-                // println!("This is a neighbor! Adding it next to the last one!");
-                acc.partial.extend(vec![next]);
-            } else {
-                // println!("Not a neighbor! Flushing partial and beginning a new one!");
-                let mut new_cells = vec![];
-                new_cells.extend(acc.partial.iter());
-                acc.partial = vec![next];
-                let num_str = new_cells
-                    .iter()
-                    .map(|cell: &&Cell| cell.content.to_string())
-                    .collect::<Vec<String>>()
-                    .join("");
-                let new_cellgroup = vec![CellGroup {
-                    cells: new_cells,
-                    num: num_str,
-                }];
-                // println!("Created: {:?}", new_cellgroup);
-                acc.nums.extend(new_cellgroup);
-            }
-        }
-    });
-    // TODO: This is just the 'flush partial' routine again...
-    let mut new_cells = vec![];
-    new_cells.extend(acc.partial.iter());
-    acc.partial = vec![];
-    let num_str = new_cells
-        .iter()
-        .map(|cell: &&Cell| cell.content.to_string())
-        .collect::<Vec<String>>()
-        .join("");
-    let new_cellgroup = vec![CellGroup {
-        cells: new_cells,
-        num: num_str, // todo: fail
-    }];
-    // println!("Created: {:?}", new_cellgroup);
-    acc.nums.extend(new_cellgroup);
-    acc.nums
-}
-
-fn does_border(g: &CellGroup, cell: &Cell) -> bool {
-    g.cells.iter().any(|gc| {
-        let x_dist = gc.x.abs_diff(cell.x);
-        let y_dist = gc.y.abs_diff(cell.y);
-        x_dist <= 1 && y_dist <= 1
-    })
-}
-
-fn is_part(g: &CellGroup, symbols: &Vec<&Cell>) -> bool {
-    symbols.iter().any(|c| does_border(g, c))
-}
+use common::CellGroup;
 
 pub fn run() {
-    let lines = include_str!("../resources/problem_3.txt").lines();
+    let schematic = common::produce_schematic();
 
-    // Parse into cells
-    let mut cells: Vec<Cell> = vec![];
-    lines.enumerate().for_each(|(lin_num, l)| {
-        l.chars().enumerate().for_each(|(char_num, c)| {
-            cells.push(Cell {
-                y: lin_num as i32,
-                x: char_num as i32,
-                content: c,
-            })
-        })
-    });
+    println!("Numbers found: {}", schematic.number_groups.len());
 
-    // Decide which ones are numbers and which are symbols
-    let (number_cells, symbol_cells, empty_cells) = partition_cell_types(&cells);
-
-    println!(
-        "Number Cells: {}, Symbol Cells: {}, Empty Cells: {}",
-        number_cells.len(),
-        symbol_cells.len(),
-        empty_cells.len()
-    );
-
-    let numbers = combine_numbers(number_cells);
-
-    println!("Numbers found: {}", numbers.len());
-
-    let part_numbers = numbers
+    let part_numbers = schematic
+        .number_groups
         .iter()
-        .filter(|&n| is_part(n, &symbol_cells))
+        .filter(|&n| common::is_part(n, &schematic.symbols))
         .collect::<Vec<&CellGroup>>();
 
     println!("Part Numbers found: {}", part_numbers.len());
