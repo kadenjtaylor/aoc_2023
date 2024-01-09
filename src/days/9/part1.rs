efbd26e553adcc9820359c144fbb246cdbfb1eaa
Kaden Taylor <kadenjtaylor@gmail.com>
2024-01-08 20:46:37 -0500

    day 9 part 2

--- src/days/9/part1.rs
+++ src/days/9/part1.rs
@@ -7,8 +7,9 @@ struct Readings {
 struct ShownWork {
     seq: Vec<i32>,
     diffs: Vec<Vec<i32>>,
-    extrapolated: Vec<i32>,
-    prediction: i32,
+    // extrapolated: Vec<i32>,
+    next_value: i32,
+    previous_value: i32,
 }
 
 fn parse(file_data: &str) -> Readings {
@@ -50,54 +51,62 @@ fn diff_vec(v: &Vec<i32>) -> Vec<Vec<i32>> {
     all_vecs
 }
 
-fn extrapolate(v: &Vec<Vec<i32>>) -> Vec<i32> {
+fn extrapolate_forward(diffs: &Vec<Vec<i32>>) -> Vec<i32> {
     let mut guesses = vec![];
-    let mut last_items = v.iter().map(|sequence| sequence.last().unwrap()).rev();
+    let mut last_items = diffs.iter().map(|sequence| sequence.last().unwrap()).rev();
     let mut current = last_items.next().unwrap();
     while let Some(item) = last_items.next() {
+        // println!("{} + {} = {}", current, item, current + item);
         guesses.push(current + item);
         current = guesses.last().unwrap();
     }
     guesses
 }
 
-fn example() -> i32 {
-    let readings = parse(test_data());
+fn extrapolate_backward(diffs: &Vec<Vec<i32>>) -> Vec<i32> {
+    let mut guesses = vec![];
+    let mut first_items = diffs.iter().map(|sequence| sequence.first().unwrap()).rev();
+    let mut current = first_items.next().unwrap();
+    while let Some(item) = first_items.next() {
+        // println!("{} - {} = {}", item, current, item - current);
+        guesses.push(item - current);
+        current = guesses.last().unwrap();
+    }
+    guesses
+}
+
+fn work_problem(readings: Readings) -> Vec<ShownWork> {
     let mut work = vec![];
     for seq in readings.sequences {
         let diffs = diff_vec(&seq);
-        let extrapolated = extrapolate(&diffs);
-        let prediction = *extrapolated.last().unwrap();
+        let next_value = *extrapolate_forward(&diffs).last().unwrap();
+        let previous_value = *extrapolate_backward(&diffs).last().unwrap();
         work.push(ShownWork {
             seq,
             diffs,
-            extrapolated,
-            prediction,
+            next_value,
+            previous_value,
         });
     }
-    work.iter().map(|work| work.prediction).sum()
+    work
+}
+
+fn example() -> Vec<ShownWork> {
+    let readings = parse(test_data());
+    work_problem(readings)
 }
 
 fn part1() -> i32 {
     let readings = parse(real_data());
-    let mut work = vec![];
-    for seq in readings.sequences {
-        let diffs = diff_vec(&seq);
-        let extrapolated = extrapolate(&diffs);
-        let prediction = *extrapolated.last().unwrap();
-        work.push(ShownWork {
-            seq,
-            diffs,
-            extrapolated,
-            prediction,
-        });
-    }
-    work.iter().map(|work| work.prediction).sum()
+    let worked = work_problem(readings);
+    worked.iter().map(|work| work.next_value).sum()
 }
 
-// fn part2() {
-//     todo!("Implement!")
-// }
+fn part2() -> i32 {
+    let readings = parse(real_data());
+    let worked = work_problem(readings);
+    worked.iter().map(|work| work.previous_value).sum()
+}
 
 fn test_data() -> &'static str {
     "0 3 6 9 12 15
@@ -110,6 +119,6 @@ fn real_data() -> &'static str {
 }
 
 pub fn run() {
-    let current = part1();
+    let current = part2();
     println!("Sum of predicted values: {}", current);
 }
